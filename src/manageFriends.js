3faa25411b6cef5b0e909ca75c66d34041165d9c
costamay <titusouko@gmail.com>
2022-09-26 17:03:26 +0300

    redux

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,32 @@
+let state = {
+  friends : []
+}
+
+let action = {
+  type : "friends/add",
+  payload: {
+    name: "Chrome Boi",
+    hometown: "NYC",
+    id: 1
+  },
+  
+  // type : "friends/remove",
+  // id: 1
+
+}
+
 export function manageFriends(state, action) {
-  // your code here
+  switch(action.type){
+    case "friends/add":
+      // let new_friends_list = [...state.friends, action.payload]
+      return {
+        state: [...state.friends, action.payload]
+      }
+    // case "friends/remove":
+    //   let updated_friends_list = state.friends.filter(friend => friend.id != action.id)
+    //   return {state: updated_friends_list} 
+    // default:
+      return state
+
+  }
 }
